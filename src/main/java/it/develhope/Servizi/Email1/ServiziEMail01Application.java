bcf93120499019ecee757d3128828d32f6a0c056
Emilio <emi00@gmail.com>
2022-11-11 12:08:42 +0100

    feat: First commit

---
+++ src/main/java/it/develhope/Servizi/Email1/ServiziEMail01Application.java
@@ -0,0 +1,13 @@
+package it.develhope.Servizi.Email1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiziEMail01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiziEMail01Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/develhope/Servizi/Email1/ServiziEMail01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.develhope.Servizi.Email1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiziEMail01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
